d85b09f4f50c78792bce8dcfaa3c9aa8dae8c771
FakhryanZ <fakhryanmuhammad@gmail.com>
2020-12-29 20:52:46 +0700

    init commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ButtonKuda;
+    public GameObject ButtonKucing;
+    public GameObject ButtonAnjing;
+    public GameObject ButtonBebek;
+    public GameObject Panel;
+    public GameObject ButtonSembunyi;
+
+    public Text Title;
+    public Text Content;
+
+    public void showButtonKuda(){
+        ButtonKuda.SetActive(true);
+    }
+
+    public void showButtonKucing(){
+        ButtonKucing.SetActive(true);
+    }
+
+    public void showButtonAnjing(){
+        ButtonAnjing.SetActive(true);
+    }
+    
+    public void showButtonBebek(){
+        ButtonBebek.SetActive(true);
+    }
+
+    public void hideButtonKuda(){
+		ButtonKuda.SetActive(false);
+        hideButton();
+	}
+
+    public void hideButtonKucing(){
+        ButtonKucing.SetActive(false);
+        hideButton();
+	}
+
+    public void hideButtonAnjing(){
+        ButtonAnjing.SetActive(false);
+        hideButton();
+	}
+
+    public void hideButtonBebek(){
+        ButtonBebek.SetActive(false);
+        hideButton();
+	}
+
+	public void hideButton(){
+        Panel.SetActive(false);
+        ButtonSembunyi.SetActive(false);
+	}
+
+    public void showKucing(){
+        Title.text="KUCING";
+        Content.text = "Kucing adalah sejenis mamalia karnivora dari keluarga felidae.";
+        showPanel();
+	}
+
+    public void showAnjing(){
+		Title.text="ANJING";
+        Content.text = "Anjing adalah mamalia yang berasal dari spesies Canis lupus familiaris.";
+        showPanel();
+	}
+
+    public void showKuda(){
+		Title.text="KUDA";
+        Content.text = "Kuda adalah salah satu dari sepuluh spesies modern mamalia dari genus Equus. Kuda dapat ditunggani oleh manusia dengan menggunakan sadel.";
+        showPanel();
+	}
+
+    public void showBebek(){
+		Title.text="BEBEK";
+        Content.text = "Bebek adalah nama umum untuk beberapa spesies burung dalam faimili Anatidae. Bebek umumnya dapat ditemukan pada perairan air tawar maupun air laut.";
+        showPanel();
+	}
+
+    public void showPanel(){
+        Panel.SetActive(true);
+        ButtonSembunyi.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/PlayAnimalSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayAnimalSound : MonoBehaviour
+{
+    public GameObject buttonBunyi;
+    public UnityEvent OnClick = new UnityEvent();
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonBunyi = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var camera = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0)){
+            if(Physics.Raycast(camera, out hit) && hit.collider.gameObject == gameObject){
+                OnClick.Invoke();
+            }
+        }
+    }
+}
